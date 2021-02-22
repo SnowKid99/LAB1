8b948ec0be6dd6adaa422b958ddf81ce22f406a2
SnowKid99 <61504008+SnowKid99@users.noreply.github.com>
2021-02-22 14:03:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# zad1
+a='napis1','napis2'
+b=21,10
+c=20.1,14.6
+d=10+3j,24-4j
+print(a)
+print(b)
+print(c)
+print(d)
+
+#zad2
+a=10
+b=5
+print(a-b)
+print(a+b)
+print(a/b)
+print(a*b)
+
+#zad3
